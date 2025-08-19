6d7476ef8c42d565833a109ba318648df4f99955
Ashton525 <ashtoncashier@gmail.comm>
2025-08-19 14:21:38 -0400

    Finished Form

--- packages/client/src/pages/Assessments/NewAssessment.jsx
+++ packages/client/src/pages/Assessments/NewAssessment.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Button, Form } from 'react-bootstrap';
 import { AssessmentService } from '../../services/AssessmentService';
 
@@ -9,7 +10,136 @@ export const NewAssessment = () => {
     await AssessmentService.submit(data);
   };
 
-  return <Form>
+  return <Form onSubmit={(handleSubmit) => handleSubmit(onSubmit)}>
+
+    <Form.Group controlId="CatName" classname="mb-3">
+      <h1>Cat Assessment Infos</h1>
+      <h2>Instrument</h2>
+      Cat Behavioral Instrument
+
+    </Form.Group>
+
+    <Form.Group controlId="CatName" classname="mb-3">
+      <h2> Cat Details </h2>
+      <Form.Label>Cat Name</Form.Label>
+      <Form.Control
+        type="text"
+        placeholder="Enter Pet Name"
+        value={Form.CatName}
+      />
+    </Form.Group>
+
+    <Form.Group controlId="CatBirthday" classname="mb-3">
+      <Form.Label>Cat Birthday</Form.Label>
+      <Form.Control
+        type="date"
+        placeholder="Enter Pet Birthday"
+        value={Form.CatBirthday}
+        required
+      />
+    </Form.Group>
+
+    <Form.Group controlId="CatJudicialContact" classname="mb-3">
+      <h2>Questions</h2>
+      <Form.Label>1. Previous contact with the Cat Judicial System</Form.Label>
+      <div>
+        <Form.Check
+          type="radio"
+          label="Yes"
+          name="CatJudicialContact"
+          id="CatJudicialContactYes"
+          value="1"
+        />
+        <Form.Check
+          type="radio"
+          label="No"
+          name="CatJudicialContact"
+          id="CatJudicialContactNo"
+          value="0"
+        />
+      </div>
+    </Form.Group>
+
+    <Form.Group controlId="PhysicalAltercation" classname="mb-3">
+      <Form.Label>2. Physical altercations with other cats</Form.Label>
+      <div>
+        <Form.Check
+          type="radio"
+          label="3+"
+          name="PhysicalAltercation"
+          id="PhysicalAltercationYes"
+          value="1"
+        />
+        <Form.Check
+          type="radio"
+          label="0-3"
+          name="PhysicalAltercation"
+          id="PhysicalAltercationNo"
+          value="0"
+        />
+      </div>
+    </Form.Group>
+
+    <Form.Group controlId="OwnerAltercations" classname="mb-3">
+      <Form.Label>3. Physical altercations with owner (scratching, biting, etc..)</Form.Label>
+      <div>
+        <Form.Check
+          type="radio"
+          label="10+"
+          name="OwnerAltercations"
+          id="OwnerAltercationsYes"
+          value="1"
+        />
+        <Form.Check
+          type="radio"
+          label="0-10"
+          name="OwnerAltercations"
+          id="OwnerAltercationsYes"
+          value="0"
+        />
+      </div>
+    </Form.Group>
+
+    <Form.Group controlId="DogPlay" classname="mb-3">
+      <Form.Label>4. Plays well with dogs.</Form.Label>
+      <div>
+        <Form.Check
+          type="radio"
+          label="Yes"
+          name="DogPlay"
+          id="DogPlayYes"
+          value="0"
+        />
+        <Form.Check
+          type="radio"
+          label="No"
+          name="DogPlay"
+          id="DogPlayNo"
+          value="1"
+        />
+      </div>
+    </Form.Group>
+
+    <Form.Group controlId="HissesStrangers" classname="mb-3">
+      <Form.Label>5. Hisses at strangers.</Form.Label>
+      <div>
+        <Form.Check
+          type="radio"
+          label="Yes"
+          name="HissesStrangers"
+          id="HissesStrangersYes"
+          value="1"
+        />
+        <Form.Check
+          type="radio"
+          label="No"
+          name="HissesStrangersNo"
+          id="HissesStrangersNo"
+          value="0"
+        />
+      </div>
+    </Form.Group>
+
     <Button variant="primary" type="submit">Submit</Button>
   </Form>;
 };
